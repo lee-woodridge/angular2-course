a97270263da1753a688d8fb437602b97029b955e
Lee Woodridge <le3wood@gmail.com>
2016-04-21 21:57:33 -0700

    First commit at the end of Section 2 of the course.

---
+++ app/authors.component.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core'
+import {AuthorService} from './author.service'
+
+@Component({
+    selector: 'authors',
+    template: `
+        <h2>{{ title }}</h2>
+        <ul>
+            <li *ngFor="#author of authors">{{ author }}</li>
+        </ul>
+        `,
+    providers: [AuthorService]
+})
+
+export class AuthorsComponent {
+    title: string = "Authors";
+    authors;
+    
+    constructor(authorService: AuthorService) {
+        this.authors = authorService.getAuthors();
+    }
+}
