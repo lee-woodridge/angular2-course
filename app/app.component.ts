ca26c7e3245193f45e286eadbb9386ef78ce62e3
Lee Woodridge <le3wood@gmail.com>
2016-04-23 12:48:26 -0700

    Added my code as I worked alongside the lectures from section 3. Added my own version of the code for the end of section task.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,16 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component';
 import {AuthorsComponent} from './authors.component';
+import {FavouriteComponent} from './favourite.component';
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>THE TOP TITLE</h1>
         <courses></courses>
-        <authors></authors>`,
-    directives: [CoursesComponent, AuthorsComponent]
+        <authors></authors>
+        <br/>
+        <favourite></favourite>`,
+    directives: [CoursesComponent, AuthorsComponent, FavouriteComponent]
 })
 export class AppComponent { }
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -5,11 +5,31 @@ import {AutoGrowDirective} from './auto-grow.directive'
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
-        <input type="text" autoGrow />
+        <h2>{{ title }}</h2> <!-- Interpolation -->
+        
+        <!-- For loop printing authors -->
         <ul>
             <li *ngFor="#course of courses">{{ course }}</li>
         </ul>
+        
+        <!-- Custom directive autoGrow (resizes on focus/unfocus) -->
+        <div>
+            <input type="text" autoGrow />
+        </div>
+        
+        <!-- Implement two-way binding manually (inline just for small example) -->
+        <div>
+            manualBind: {{ manualBind }}
+            <input type="text" [value]="manualBind" (input)="manualBind = $event.target.value" />
+            <button (click)="manualBind = ''">Clear</button>
+        </div>
+        
+        <!-- Implement two-way binding with ngModel -->
+        <div>
+            autoBind: {{ autoBind }}
+            <input type="text" [(ngModel)]="autoBind" /> <!-- [(ngModel)] == bindon-ngModel -->
+            <button (click)="autoBind = ''">Clear</button>
+        </div>
         `,
     providers: [CourseService],
     directives: [AutoGrowDirective]
@@ -18,6 +38,8 @@ import {AutoGrowDirective} from './auto-grow.directive'
 export class CoursesComponent {
     title: string = "Courses";
     courses;
+    manualBind: string = "starting value";
+    autoBind: string = "starting value";
     
     constructor(courseService: CourseService) {
         this.courses = courseService.getCourses();
