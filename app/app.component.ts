36aeaeb7f21a40fd6804fe2c56a24ad2dee32ede
Lee Woodridge <le3wood@gmail.com>
2016-04-24 13:58:03 -0700

    Added lecture 44 and 45: ngSwitch and ngFor.

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,10 +12,44 @@ import {Component} from 'angular2/core';
         <div *ngIf="courses.length==0">
             You have no courses yet.
         </div>
+
+        <ul class="nav nav-pills">
+            <!-- style as active when selected, and change viewMode variable -->
+            <li [class.active]="viewMode == 'map'">
+                <a (click)="viewMode = 'map'">Map View</a>
+            </li>
+            <li [class.active]="viewMode == 'list'">
+                <a (click)="viewMode = 'list'">List View</a>
+            </li>
+        </ul>
+        <!-- switch on the variable viewMode -->
+        <div [ngSwitch]="viewMode">
+            <!-- template is html5: not run/rendered until activated -->
+            <!-- ngSwitchWhen is like case from normal switch -->
+            <!-- ngSwitchDefault is like the default case statement -->
+            <template [ngSwitchWhen]="'map'" ngSwitchDefault>Map View Content</template>
+            <template [ngSwitchWhen]="'list'">List View Content</template>
+        </div>
+
+        <!-- ngFor example showing index as well as course -->
+        <ul>
+            <li *ngFor="#course of courses, #i = index">
+                {{ i+1 }}) {{ course }}
+            </li>
+        </ul>
+
+        <!-- the * tells angular to generate the li wrapped in a template, like:
+        <ul>
+            <template ngFor [ngForOf]="courses" #course #i=index>
+                <li>{{ i + 1 }}) {{ course }}</li>
+            </template>
+        </ul>
+        -->
         `,
     directives: []
 })
 
 export class AppComponent {
-    courses: string[] = [];
+    courses: string[] = ['Course 1', 'Course 2', 'Course 3'];
+    viewMode;
 }
