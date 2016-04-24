7389ff1528a6352027976b605df71f452152b9df
Lee Woodridge <le3wood@gmail.com>
2016-04-24 10:45:54 -0700

    Lecture 43: hiding dom elements - ngIf and [hidden].

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,14 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div>
+        <!-- Hiding a div by binding to the hidden attribute -->
+        <!-- This creates the div and hides it if true -->
+        <div [hidden]="courses.length==0">
             Courses
         </div>
-        <div>
+        <!-- Hiding a div by using the ngIf directive -->
+        <!-- This does not create the div at all if false -->
+        <div *ngIf="courses.length==0">
             You have no courses yet.
         </div>
         `,
@@ -13,5 +17,5 @@ import {Component} from 'angular2/core';
 })
 
 export class AppComponent {
-    
+    courses: string[] = [];
 }
