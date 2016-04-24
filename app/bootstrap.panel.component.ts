104ee0fc7f7dffa7fe78f9add65b83cae3be9120
Lee Woodridge <le3wood@gmail.com>
2016-04-24 15:33:03 -0700

    Setup for lecture 50: added bootstrap panel component.

---
+++ app/bootstrap.panel.component.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'bs-panel',
+    template: `
+        <div class="panel panel-default">
+            <div class="panel-heading"></div>
+            <div class="panel-body"></div>
+        </div>
+    `
+})
+
+export class BootstrapPanelComponent {
+}
