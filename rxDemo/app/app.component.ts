8726d9670f01d640a8174fa29a598c12e2881572
Lee Woodridge <le3wood@gmail.com>
2016-04-28 16:01:16 -0700

    Added lecture 82-86: replacing the callback version with observables. Showing map, filter, debounce, distinct, flatMap, subscribe and unsubscribe on Observables.

--- rxDemo/app/app.component.ts
+++ rxDemo/app/app.component.ts
@@ -2,6 +2,8 @@
 
 import {Component} from 'angular2/core';
 
+import {Observable} from 'rxjs/Rx';
+
 @Component({
     selector: 'my-app',
     template: `
@@ -10,22 +12,38 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     constructor(){
-        // Create a debouncer, so the max freq we call server is 400ms.
-        var debounced = _.debounce(function(text){
-            var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
-            $.getJSON(url, function(artists) {
-                console.log(artists);
-            });
-        }, 400);
+        var keyups = Observable.fromEvent($("#search"), "keyup") // subscribe to keyups.
+            .map(e => e.target.value) // map from DOM event to string
+            .filter(s => s.length >= 3) // filter for only strings >= 3
+            .debounceTime(400) // debounce for 400ms
+            .distinctUntilChanged() // call only once per input.
+            .flatMap(s => {
+                var url = "https://api.spotify.com/v1/search?type=artist&q=" + s;
+                var promise = $.getJSON(url); // get the promise from getJSON
+                return Observable.fromPromise(promise); // subscribe to the promise
+            }); // flatMap flattens our Observable of Observables
+            // into an Observable which returns the data.
+            // In production, the code inside flatMap should be refactored into a service class.
 
-        $("#search").keyup(function(e){
-            var text = e.target.value;
+        var subscription = keyups.subscribe(data => console.log(data));
+        subscription.unsubscribe();
 
-            // To not flood server.
-            if(text.length < 3)
-                return;
+        // // Create a debouncer, so the max freq we call server is 400ms.
+        // var debounced = _.debounce(function(text){
+        //     var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+        //     $.getJSON(url, function(artists) {
+        //         console.log(artists);
+        //     });
+        // }, 400);
 
-            debounced(text);
-        });
+        // $("#search").keyup(function(e){
+        //     var text = e.target.value;
+
+        //     // To not flood server.
+        //     if(text.length < 3)
+        //         return;
+
+        //     debounced(text);
+        // });
     }
 }
