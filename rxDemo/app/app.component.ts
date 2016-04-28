7215819061b9fd22d24b541618dbba67913e2670
Lee Woodridge <le3wood@gmail.com>
2016-04-28 15:14:36 -0700

    Added lecture 81: creating an artist search autocomplete using jquery and underscore.

--- rxDemo/app/app.component.ts
+++ rxDemo/app/app.component.ts
@@ -10,5 +10,22 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     constructor(){
+        // Create a debouncer, so the max freq we call server is 400ms.
+        var debounced = _.debounce(function(text){
+            var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+            $.getJSON(url, function(artists) {
+                console.log(artists);
+            });
+        }, 400);
+
+        $("#search").keyup(function(e){
+            var text = e.target.value;
+
+            // To not flood server.
+            if(text.length < 3)
+                return;
+
+            debounced(text);
+        });
     }
 }
