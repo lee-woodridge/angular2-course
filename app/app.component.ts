7437e015d6f99b7f4fa3aacffefbf6a33fea312b
Lee Woodridge <le3wood@gmail.com>
2016-04-30 12:53:12 -0700

    Clean directory for new section.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,9 @@
 import {Component} from 'angular2/core';
-import {SignUpFormComponent} from './signup-form.component'
-import {ChangePasswordComponent} from './change-password.component'
 
 @Component({
     selector: 'my-app',
-    directives: [SignUpFormComponent, ChangePasswordComponent],
+    directives: [],
     template: `
-        <signup-form></signup-form>
-        <br/>
-        <br/>
-        <br/>
-        <change-password></change-password>
     `
 })
 export class AppComponent {
