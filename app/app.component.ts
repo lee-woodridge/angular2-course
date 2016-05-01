75c6738541338a8e05706a2c7d2bfc790c83e36d
Lee Woodridge <le3wood@gmail.com>
2016-05-01 14:25:48 -0700

    Setup base project for starting the class final project.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,55 +1,25 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {AlbumsComponent} from './albums.component';
-import {AlbumComponent} from './album.component';
-import {ArchivesComponent} from './archives.component';
-import {ArchiveComponent} from './archive.component';
-import {ContactComponent} from './contact.component';
+import {PostsComponent} from './posts.component';
+import {UsersComponent} from './users.component';
 
 @RouteConfig([
-    // These routes are all complete, but for using domain routing we
-    // could delegate a route by ending path with ..., for example:
-    // {
-    //     path: '/event/...',
-    //     component: EventComponent
-    // }
-    // would delegate anything after /event/ to EventComponent,
-    // which would contain a route like:
-    // {
-    //     path: '/:id'
-    // }
-    // so /event/1 would drop into EventComponent route :id.
     {
-        path: '/archives',
-        name: 'Archives',
-        component: ArchivesComponent,
+        path: '/users',
+        name: 'Users',
+        component: UsersComponent,
         useAsDefault: true
     },
     {
-        path: '/archives/:year/:month',
-        name: 'Archive',
-        component: ArchiveComponent
-    },
-    {
-        path: '/albums',
-        name: 'Albums',
-        component: AlbumsComponent
-    },
-    {
-        path: '/albums/:id', // :id is route parameter
-        name: 'Album',
-        component: AlbumComponent
-    },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: ContactComponent
+        path: '/posts',
+        name: 'Posts',
+        component: PostsComponent
     },
     {
         path: '/*other', // wildcard route, with redirect
         name: 'Other',
-        redirectTo: ['Archives']
+        redirectTo: ['Users']
     }
 ])
 @Component({
