98e2847dee3e258bd33f3bf0738c09c75deea249
Lee Woodridge <le3wood@gmail.com>
2016-05-01 01:17:39 -0700

    Added lecture 111: programmatic navigation.

--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,10 +1,18 @@
 import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
 
 @Component({
     templateUrl: '/app/contact.component.html'
 })
 export class ContactComponent {
+    constructor(private _router: Router) {
+
+    }
+
     onSubmit(form){
         console.log(form);
+        // Below is code which controls the router, navigating to Albums on form submission.
+        // This is called imperative or programmatic navigation.
+        this._router.navigate(['Albums']);
     }
 }
