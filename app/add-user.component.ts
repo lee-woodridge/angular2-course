b010b54db25bb081b33b077e14c98ae2c6cdaec8
Lee Woodridge <le3wood@gmail.com>
2016-05-03 16:40:43 -0700

    Added dirty tracking for our form (error box on navigating away if form has content).

--- app/add-user.component.ts
+++ app/add-user.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {Control, ControlGroup, Validators, FormBuilder} from 'angular2/common';
+import {CanDeactivate} from 'angular2/router';
 
 import {EmailValidator} from './emailValidator';
 
@@ -8,7 +9,7 @@ import {EmailValidator} from './emailValidator';
     templateUrl: './app/add-user.component.html'
 })
 
-export class AddUserComponent {
+export class AddUserComponent implements CanDeactivate {
     form: ControlGroup;
 
     constructor(private _fb: FormBuilder) {
@@ -27,5 +28,13 @@ export class AddUserComponent {
 
     submit() {
         console.log(this.form);
+    }
+
+    routerCanDeactivate() {
+        if(this.form.dirty) {
+            return confirm('Are you sure?');
+        }
+
+        return true;
     }
 }
